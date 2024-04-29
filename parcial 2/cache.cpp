882b094aa12ec1a3848601e96f7a914ee85bb40f
isabel-olivero <124202028+isabel-olivero@users.noreply.github.com>
2024-04-29 02:05:13 -0500

    Add files via upload

--- parcial 2/cache.cpp
+++ parcial 2/cache.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <stack>
+#include <pair>
 #include <unordered_map>
 
 using namespace std;
@@ -10,9 +11,12 @@ using namespace std;
 
 class Cache {
     private:
-        unordered_map <int,int> mem; /*la clave es la poscicion y el valor es el dato */
+        unordered_map <string,pair<string, string>> mem; /*la clave es la etiqueta, el valor 2 es el dato y el valor 1 el desplazamieno */
         int tamaño;
     public:
+    void LRU();  /*en esta opcion seria hacer una cola con las pocisiones usadas y reemplazar la que salga al sacar de la cola*/
+    string read();
+    void write();
 
 
 }
@@ -21,15 +25,20 @@ class Cache {
 /*opcion 2 implemenacion con clases  y vectores */
 class Cache {
     private:
-        vector <int,int> mem;
+        vector  <string,pair<string, string>> mem;
         int tamaño;
     public:
-
+    void LRU(); /*en esta opcion seria reemplazar la primera posicion de la lista*/
+    string read();
+    void write();
 
 }
 
 /*opcion 3 implemenacion con  vectores */
 
 vector <int,int> cache;
+void LRU(vector<string,pair<string, string>> cache ); /*en esta opcion seria reemplazar la primera posicion de la lista*/
+string read();
+void write();
 
 
