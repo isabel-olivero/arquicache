f0f663c72cba3c33437f864e866668998376001f
isabel-olivero <124202028+isabel-olivero@users.noreply.github.com>
2024-04-29 01:30:46 -0500

    Add files via upload

--- parcial 2/cache.cpp
+++ parcial 2/cache.cpp
@@ -2,5 +2,34 @@
 #include <vector>
 #include <string>
 #include <stack>
+#include <unordered_map>
 
 using namespace std;
+
+/*opcion 1 implemenacion con clases  y mapas*/
+
+class Cache {
+    private:
+        unordered_map <int,int> mem; /*la clave es la poscicion y el valor es el dato */
+        int tamaño;
+    public:
+
+
+}
+
+
+/*opcion 2 implemenacion con clases  y vectores */
+class Cache {
+    private:
+        vector <int,int> mem;
+        int tamaño;
+    public:
+
+
+}
+
+/*opcion 3 implemenacion con  vectores */
+
+vector <int,int> cache;
+
+
