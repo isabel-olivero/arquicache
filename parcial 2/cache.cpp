a9be70ec342bec2d9ffb3be0399a7c4035ef0697
isabel-olivero <124202028+isabel-olivero@users.noreply.github.com>
2024-04-29 22:55:44 -0500

    Add files via upload

--- parcial 2/cache.cpp
+++ parcial 2/cache.cpp
@@ -9,38 +9,47 @@ using namespace std;
 
 /*la memoria principal si debe ser un mapa ya que es necesario relacionar dato con direccion*/
 
-/*opcion 1 implemenacion con clases  y mapas*/
+/*opcion 1 implemenacion con clases  y mapas
 
 class Cache {
     private:
-        unordered_map <string,pair<string, string>> mem; /*la clave es la etiqueta, el valor 2 es el dato y el valor 1 el desplazamieno */
+        unordered_map <string,pair<string, string>> mem; /*la clave es la etiqueta, el valor 2 es el dato y el valor 1 el desplazamieno 
         int tamaño;
     public:
-    void LRU();  /*en esta opcion seria hacer una cola con las pocisiones usadas y reemplazar la que salga al sacar de la cola*/
+    void LRU();  /*en esta opcion seria hacer una cola con las pocisiones usadas y reemplazar la que salga al sacar de la cola
     string read();
     void write();
 
 
-}
+}*/
 
 
-/*opcion 2 implemenacion con clases  y vectores */
+/*opcion 2 implemenacion con clases  y vectores */ 
 class Cache {
     private:
-        vector  <string,pair<string, string>> mem;
+        vector  <vector<string,string, string>> mem; /*bit de validez, adress, data*/
         int tamaño;
     public:
-    void LRU(); /*en esta opcion seria reemplazar la primera posicion de la lista*/
+    Cache(int tam){
+        tamaño = tam;
+        mem.resize(tamaño);
+        for(int i = 0; i < tamaño; ++i){
+            mem[i][0] = '0';
+        }
+    }
+    void LRU(string dato); /*en esta opcion seria reemplazar la primera posicion de la lista*/
     string read();
     void write();
 
 }
 
-/*opcion 3 implemenacion con  vectores */
+
+
+/*opcion 3 implemenacion con  vectores 
 
 vector <int,int> cache;
-void LRU(vector<string,pair<string, string>> cache ); /*en esta opcion seria reemplazar la primera posicion de la lista*/
+void LRU(vector<string,pair<string, string>> cache ); /*en esta opcion seria reemplazar la primera posicion de la lista
 string read();
 void write();
-
+*/
 
